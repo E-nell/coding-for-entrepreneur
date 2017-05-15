b85ff85175ab105cf61e50fc64a5af3a624fbcac
JW <dlwlxndj@naver.com>
2017-05-15 17:39:07 +0900

    Add files via upload

---
+++ src/CollectionFrameworks.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * Array
+ * ArrayList
+ * HashSet
+ * HashMap
+ */
+public class CollectionFrameworks {
+    public void explainArray(){
+        int[] intArray = new int[5];
+        // double[] doubleArray = new double[5];
+
+        intArray[0] = 5;
+        System.out.println(intArray[0]); //1
+        System.out.println(intArray[1]); //2
+        System.out.println(intArray[2]); //3
+        System.out.println(intArray[3]); //4
+        System.out.println(intArray[4]); //5
+    }
+    public void explainArrayList(){
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+
+        System.out.println(arrayList.size());
+        System.out.println(arrayList.get(0));
+    }
+    public void explainHashSet(){
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.add("hello");
+        hashSet.add("jaewoo");
+        hashSet.add("hello");
+
+        System.out.println(hashSet.size());
+        System.out.println(hashSet.contains("hello"));
+        System.out.println(hashSet.contains("okay"));
+
+        ArrayList<String> arrayList = new ArrayList<String>(hashSet);
+        arrayList.get(0);
+        System.out.println(arrayList.get(0));
+    }
+    public void explainHashMap() {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("name", "Jaewoo");
+        System.out.println(hashMap.get("name"));
+    }
+}
