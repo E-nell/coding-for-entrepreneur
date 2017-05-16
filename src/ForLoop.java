df44c841e30ffd78fce1e69533cef6e2a1337fc4
JW <dlwlxndj@naver.com>
2017-05-16 17:54:08 +0900

    Add files via upload

---
+++ src/ForLoop.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * for-loop
+ * - original
+ * - for-each
+ * while
+ */
+public class ForLoop {
+    public void explainForLoop(){
+        String[] strings = {"hello", "this", "is", "Jaewoo",
+                "hello", "this", "is", "Jaewoo",
+                "hello", "this", "is", "Jaewoo",
+                "hello", "this", "is", "Jaewoo",
+        };
+
+
+        //System.out.println("hello");
+        //System.out.println("this");
+        //System.out.println("is");
+        //System.out.println("Jaewoo");
+
+        int testInt = 3;
+        //testInt = testInt + 1;
+        testInt ++;
+        System.out.println(testInt);
+
+        for(int i=0; i < strings.length; i++ ) {
+            System.out.println(strings[i]);
+        }
+
+    }
+    public void explainForLoopWithArrayList(){
+        String[] strings = {"hello", "this", "is", "Jaewoo"};
+        ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(strings));
+        arrayList.add("!");
+        for(int i=0; i < arrayList.size(); i++ ) {
+            System.out.println((i + 1) + ". " +arrayList.get(i));
+
+        }
+        for (String targetString: arrayList){
+            System.out.println(targetString);
+        }
+
+
+
+
+    }
+    public void explainWhile() {
+        int testInt = 0;
+        while (ture) {
+            System.out.println(testInt);
+        }
+        }
+    }
+
