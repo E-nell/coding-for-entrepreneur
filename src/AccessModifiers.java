a44d584c56d4d2a5a7197b15a2a22215d87611ed
JW <dlwlxndj@naver.com>
2017-05-19 17:49:05 +0900

    Add files via upload

---
+++ src/AccessModifiers.java
@@ -0,0 +1,24 @@
+/**
+ * access modifiers; public, protected, (dafault), private
+ * getter, setter
+ */
+public class AccessModifiers {
+    int a;
+    int b;
+    public AccessModifiers(int i, int i1) {
+        this.a = a;
+        this.b = b;
+    }
+    public int sum() {
+        return a + b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
+
